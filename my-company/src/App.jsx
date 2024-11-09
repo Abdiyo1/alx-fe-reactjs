089673c1c9b751518c6688f421fc7c77bbfed9c5
Abdulsemed Adem <fedluabdi603@gmail.com>
2024-11-09 02:55:26 -0800

    move componentes to singel folder

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import Home from './Home'
-import About from './About'
-import Services from './Services'
-import Contact from './Contact'
+import Home from './components/Home'
+import About from './components/About'
+import Services from './components/Services'
+import Contact from './components/Contact'
 
 
 function App() {
