366390b305b180de6937a64fc897917070d14806
Abdulsemed Adem <fedluabdi603@gmail.com>
2024-11-09 01:19:54 -0800

    remove default export

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,9 +1,7 @@
-// UserContext.js
-
 import React from 'react';
 
-// Create the Context
-const UserContext = React.createContext();
 
-// Export the Context
-export default UserContext;
+export const UserContext = React.createContext();
+
+
+ 
