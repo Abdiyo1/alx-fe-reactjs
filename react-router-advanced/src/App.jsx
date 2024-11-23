940b5c205829797330e0844d4cea02c9540302d6
Abdulsemed Adem <fedluabdi603@gmail.com>
2024-11-23 04:34:20 -0800

    useAuth

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -39,31 +39,31 @@ import Profile from "./components/Profile";
 import Login from "./pages/Login";
 import BlogPost from "./pages/BlogPost";
 import ProtectedRoute from "./components/ProtectedRoute";
+import { useAuth } from "./context/AuthContext";
 
 function App() {
-  const isAuthenticated = true; // Simulated login status
+  const { login, logout, isAuthenticated } = useAuth();
     return (
-        <Router>
-           <nav>
-         <Link to="/"> Home </Link>
-         <Link to="login"> Login </Link>
-         <Link to="profile"> profile </Link>
-         <Link to="blog/:id">blog</Link>      
-       </nav>
+      <>
+         <Router>
+            <button onClick={login}>Login</button>
+            <button onClick={logout}>Logout</button>
+            <p>{isAuthenticated ? "Logged In" : "Logged Out"}</p>
+
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/profile/*" element={<Profile />} />
+                <Route
+                    path="/profile/*"
+                    element={
+                        <ProtectedRoute>
+                            <Profile />
+                        </ProtectedRoute>
+                    }
+                />
                 <Route path="/login" element={<Login />} />
-                <Route path="/blog/:id" element={<BlogPost />} />
-                <Route path="/profile/*"
-                          element={
-                          <ProtectedRoute isAuthenticated={isAuthenticated}>
-                              <Profile />
-                          </ProtectedRoute>
-                        }
-                />;
             </Routes>
-        </Router>
+            </Router>
+        </>
     );
 }
 
--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
-function ProtectedRoute({ isAuthenticated, children }) {
+function ProtectedRoute({ children }) {
+    const { isAuthenticated } = useAuth();
+
     if (!isAuthenticated) {
         return <Navigate to="/login" />;
     }
