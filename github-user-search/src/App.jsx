a5180ef5f6fbdfbfb6f2a3a4b24a8a2396a4045c
Abdulsemed Adem <fedluabdi603@gmail.com>
2024-12-15 21:27:32 -0800

    Initial setup for GitHub User Search Application

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold text-center">GitHub User Search</h1>
+      {/* Add components like SearchBar and UserResults later */}
+    </div>
+  );
+};
+
+export default App;
